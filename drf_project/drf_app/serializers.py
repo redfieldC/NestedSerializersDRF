365c526bb61ae89f8562f4fed5c788418eddbfca
Ameya Potdar <ameyapradippotdar@gmail.com>
2024-11-09 18:04:49 +0530

    Fix: Return only color name in product serializer response

--- drf_project/drf_app/serializers.py
+++ drf_project/drf_app/serializers.py
@@ -3,7 +3,7 @@ from .models import *
 class ColorSerializers(serializers.Serializer):
     id=serializers.UUIDField(read_only=True)
     name=serializers.CharField(max_length=20)
-    description=serializers.CharField()
+    description=serializers.CharField(default="No description")
 
     def create(self,validated_data):
         return Color.objects.create(**validated_data)
@@ -23,34 +23,34 @@ class ProductSerializers(serializers.Serializer):
     color=ColorSerializers()
 
     def create(self, validated_data):
-        color_name = validated_data.pop("color")["name"]
+        color_data = validated_data.pop("color")  # color is now an object, but you want to use only name
+        color_name = color_data.get("name")  # Extract the color name from the input data
         try:
-            color = Color.objects.get(name=color_name)
+            color = Color.objects.get(name=color_name)  # Look up the Color object using the name
             product = Product.objects.create(color=color, **validated_data)
         except Color.DoesNotExist:
             raise serializers.ValidationError({"color": f"Color {color_name} does not exist"})
         except Exception as e:
             raise serializers.ValidationError({"product": f"An error occurred while creating the product: {str(e)}"})
-
-        
-
         return product
-    
-    def update(self,instance,validated_data):
-        color_name=validated_data.pop("color",None)
-        if color_name:
+
+    def update(self, instance, validated_data):
+        color_data = validated_data.pop("color", None)
+        if color_data:
+            color_name = color_data.get("name")  # Extract the name from the color object
             try:
-                color=Color.objects.get(name=color_name["name"])
-                instance.color=color 
+                color = Color.objects.get(name=color_name)  # Look up the Color object
+                instance.color = color
             except Color.DoesNotExist:
-                raise serializers.ValidationError({"color":f"Color {color_name['name']} does not exist"})
+                raise serializers.ValidationError({"color": f"Color {color_name} does not exist"})
         
-        for attr,value in validated_data.items():
-            setattr(instance,attr,value)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
         instance.save()
         return instance
-    
-    def to_representation(self,instance):
+
+    def to_representation(self, instance):
         representation = super().to_representation(instance)
-        representation["color"]=instance.color.name 
+        # When returning the data, we will send the full color object as defined in ColorSerializer
+        representation["color"] = instance.color.name 
         return representation
